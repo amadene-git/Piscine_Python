a4ca47fa4ea7cc81455e4cd00cc729362e880303
“amadene-git” <adioumd@hotmail.fr>
2022-12-12 19:54:13 +0100

    Mon Dec 12 01:15:41 PM CET 2022

--- Python_02/ex02/logger.py
+++ Python_02/ex02/logger.py
@@ -2,26 +2,22 @@ import time
 from random import randint
 import os
 
-def deco(cl):
-	print("ut")
-	return cl
-
-# @deco
-class a():
-	def __init__(self) -> None:
-		print("salut")
-		pass
-a = deco(a)
-
-l = a()
-l = a()
-
 #... your definition of log decorator...
-def log(function):
-	def 
-	print(function.__name__)
-	os.system(f'echo "{os.environ["USER"]} {function.__name__}" >> machine.log')
-	return function
+def log(funct):
+	def deco(*args, **kwargs):
+		t_zero = time.clock_gettime(time.CLOCK_REALTIME)
+		ret = funct(*args, **kwargs)
+		t_one = time.clock_gettime(time.CLOCK_REALTIME)
+		t_format = t_one - t_zero
+		if (t_format > 0.001):
+			t_format = f"exec-time = {t_format:.3f} s"
+		else:
+			t_format = f"exec-time = {round(t_format * 1000, 3):.3f} ms"
+		os.system(f"echo \"({os.environ.get('USER', '$USER is empty')}) Running: {funct.__name__.replace('_', ' ').title():15s}[ {t_format} ]\" >> machine.log")
+		return ret
+	
+	return (deco)
+
 
 class CoffeeMachine():
 	water_level = 100
@@ -33,10 +29,12 @@ class CoffeeMachine():
 		else:
 			print("Please add water!")
 			return False
-
+	# start_machine = log(start_machine)
+	
 	@log
 	def boil_water(self):
 		return "boiling..."
+	# boil_water = log(boil_water)
 	
 	@log
 	def make_coffee(self):
@@ -46,23 +44,23 @@ class CoffeeMachine():
 				self.water_level -= 1
 			print(self.boil_water())
 			print("Coffee is ready!")
+	# make_coffee = log(make_coffee)
 	
 	@log
 	def add_water(self, water_level):
 		time.sleep(randint(1, 5))
 		self.water_level += water_level
 		print("Blub blub blub...")
+	# add_water = log(add_water)
+
+
+
 
 if __name__ == "__main__":
-	print("************************")
 	machine = CoffeeMachine()
-	# print("************************")
 	for i in range(0, 5):
-		# print("************************")
 		machine.make_coffee()
-		# print("************************")
-	
-	print("************************")
 	machine.make_coffee()
-	print("************************")
 	machine.add_water(70)
+
+
