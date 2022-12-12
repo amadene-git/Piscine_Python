f860790fe5d2e2ab5628d889dc6146359d0b2bb2
“amadene-git” <adioumd@hotmail.fr>
2022-12-12 20:56:07 +0100

    csv reader

--- Python_02/ex03/csvreader.py
+++ Python_02/ex03/csvreader.py
@@ -1,20 +1,83 @@
 class CsvReader():
-	def __init__(self, filename=None, sep=’,’, header=False, skip_top=0, skip_bottom=0):
+	def __init__(self, filename=None, sep=',', header=False, skip_top=0, skip_bottom=0):
+		print("__init__")
+		
+		try:
+			self.fileobj = open(filename, "r")
+		except FileNotFoundError:
+			print("CsvReader error: File not found", file=sys.stderr)
+			return None
+		except PermissionError:
+			print("CsvReader error: Permossion denied", file=sys.stderr)
+			return None
+
+		self.filename		= filename
+		self.sep 			= sep=','
+		self.header 		= header=False
+		self.skip_top 		= skip_top=0
+		self.skip_bottom 	= skip_bottom=0
+		self.data			= []
+		
+		self.text = self.fileobj.read()
+		
+	def __enter__(self):
 		# ... Your code here ...
-	def __enter__(...):
+		print("__enter__")
+		if not hasattr(self, 'fileobj'):
+			print ("Error fileobj")
+			return None
+		
+		self.row = self.text.split("\n")
+
+		for i in self.row:
+			self.data = i.split(",")
+		
+		return self.fileobj
+
+
+	def __exit__(self, exc_type, exc_val, exc_tb):
 		# ... Your code here ...
-	def __exit__(...):
-		# ... Your code here ...
+		print("__exit__")
+		if not hasattr(self, 'fileobj'):
+			print ("Error fileobj")
+			return None
+
+
+		self.fileobj.close()
+	
+	
 	def getdata(self):
-""" Retrieves the data/records from skip_top to skip bottom.
-Return:
-nested list (list(list, list, ...)) representing the data.
-"""
-	# ... Your code here ...
+		""" Retrieves the data/records from skip_top to skip bottom.
+		Return:
+		nested list (list(list, list, ...)) representing the data.
+		"""
+		# ... Your code here ...
+	
+	
 	def getheader(self):
-""" Retrieves the header from csv file.
-Returns:
-list: representing the data (when self.header is True).
-None: (when self.header is False).
-"""
+		""" Retrieves the header from csv file.
+		Returns:
+		list: representing the data (when self.header is True).
+		None: (when self.header is False).
+		"""
 	# ... Your code here ...
+
+
+class File(object):
+    def __init__(self, file_name, method):
+        self.file_obj = open(file_name, method)
+    def __enter__(self):
+        return self.file_obj
+    def __exit__(self, type, value, traceback):
+        print("Exception has been handled")
+        self.file_obj.close()
+        return True
+
+import sys
+
+if __name__ == "__main__":
+	with CsvReader(sys.argv[1]) as file:
+		print(f"CsvReader({sys.argv[1]})")
+
+# with File(sys.argv[1], 'r') as opened_file:
+# 	print("file opened")
